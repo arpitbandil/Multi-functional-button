5b65b5507e11cc4b801acf78eb9255546bf79f3e
Arpit Bandil <arpit@goball.com>
2024-01-14 03:53:12 +0530

    Add publishing in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,11 @@ android {
             )
         }
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
