a6a5a2d347ab03cdb7f915a172d1720472954f3f
Arpit Bandil <arpit@goball.com>
2024-01-14 06:03:40 +0530

    Added maven publish in library

--- multifunctionalbutton/build.gradle.kts
+++ multifunctionalbutton/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -29,6 +30,29 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.arpitbandil"
+                version = "1.0.2"
+            }
+        }
+
+        repositories {
+            maven {
+                url = uri("${project.buildDir}/repo")
+            }
+        }
     }
 }
 
